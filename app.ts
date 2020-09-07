2f244add33672ae101554e9de19351de7ebe8eaf
Jeremy Deutsch <jeremy.e.deutsch@gmail.com>
2020-09-06 22:33:02 -0700

    listen to incoming requests

--- app.ts
+++ app.ts
@@ -5,3 +5,11 @@ const app = express();
 app.use((req, res) => {
   return res.redirect(302, "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 });
+
+app.get("/", (req, res) => {
+  res.send("Hello World! You should never see this!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});

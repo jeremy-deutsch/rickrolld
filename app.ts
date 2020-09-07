81298ef6ec313162fe46e1c10964e029a53daf03
Jeremy Deutsch <jeremy.e.deutsch@gmail.com>
2020-09-06 22:35:39 -0700

    listen on port from env

--- app.ts
+++ app.ts
@@ -10,6 +10,8 @@ app.get("/", (req, res) => {
   res.send("Hello World! You should never see this!");
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("Listening on port " + port);
 });
